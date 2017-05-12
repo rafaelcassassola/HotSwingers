e19dfee8f5c68c9fb660f9e7d23f5ba30c3d56ad
rafaelcassassola <rafael.cassassola@gmail.com>
2017-05-12 14:47:33 -0300

    estrutura de sass

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require ('gulp-ruby-sass');
+var watch = require ('gulp-watch');
+
+gulp.task ('sass', function(){
+	return sass('sass/*.sass'). pipe (gulp.dest('css'))
+
+});
+
+gulp.task ('watch', function(){
+	gulp.watch('sass/*.sass', ['sass'])
+
+});
+
+gulp.task ('default', ['sass', 'watch']);
